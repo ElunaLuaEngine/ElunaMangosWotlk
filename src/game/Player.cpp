603147fc943545258d82aba894c598d3115780d8
Rochet2 <tqbattlenet@gmail.com>
2015-02-13 20:59:45 +0200

    Eluna: Fix OnSave hook
    - followed faulty model from old TC code fixed 6 months ago
    
    Conflicts:
    	src/game/Player.cpp

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1349,8 +1349,6 @@ void Player::Update(uint32 update_diff, uint32 p_time)
         if (update_diff >= m_nextSave)
         {
             // m_nextSave reseted in SaveToDB call
-            // used by eluna
-            sEluna->OnSave(this);
             SaveToDB();
             DETAIL_LOG("Player '%s' (GUID: %u) saved", GetName(), GetGUIDLow());
         }
@@ -17272,6 +17270,10 @@ void Player::SaveToDB()
     outDebugStatsValues();
 
     CharacterDatabase.BeginTransaction();
+
+    // Hack to check that this is not on create save
+    if (!HasAtLoginFlag(AT_LOGIN_FIRST))
+        sEluna->OnSave(this);
 
     static SqlStatementID delChar ;
     static SqlStatementID insChar ;
